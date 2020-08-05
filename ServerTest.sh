244f16aa9041c020b28e6ff803af330c2fa17f6a
root <root@localhost.localdomain>
2020-08-05 08:53:38 -0700

    Server Test 2 is required

--- ServerTest.sh
+++ ServerTest.sh
@@ -1,12 +1,12 @@
-echo "thread1"
+echo "thread10"
 date
-sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_1.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 1 > /dev/null
+sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_10.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 10 > /dev/null
 date
-echo "thread3"
+echo "thread20"
 date
-sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_3.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 3 > /dev/null
+sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_20.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 20 > /dev/null
 date
-echo "thread5"
+echo "thread30"
 date
-sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_5.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 5 >> /dev/null
+sudo ./MEGASOFT/MEGASOFT.o --input ./TestData/performance_test/inputMS_1000x40.txt --pvalue_table ./TestData/HanEskinPvalueTable.txt --output ./ServerTestResult/posterior_1000x40_30.txt --log ./TestData/log_c++_static.txt --mvalue --mvalue_method mcmc --mcmc_sample 1000000 --seed 0 --mvalue_prior_sigma 0.05 --mvalue_prior_alpha 1 --mvalue_prior_beta 5 --mvalue_p_thres 1.0 --thread 30 >> /dev/null
 date
