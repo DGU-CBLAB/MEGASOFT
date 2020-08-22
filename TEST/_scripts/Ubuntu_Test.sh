f6f4f0ae06ba46b08935bd0ed49b5dbc897f9cdd
JuhunC <a71790890@gmail.com>
2020-08-22 09:16:33 -0700

    Editting Script File

--- TEST/_scripts/Ubuntu_Test.sh
+++ TEST/_scripts/Ubuntu_Test.sh
@@ -1,3 +1,6 @@
+#
+#!/bin/bash
+
 $TestSettings
 NumThreads=(1 3 5 10 15 20 25)
 StrDate=$(date +'%F')
@@ -10,8 +13,8 @@ DirInput="../input_samples/"
 DirMEGA="./MEGASOFT.o"
 
 $DataSet
-NumStudies=8
-NumSNPs=1000
+NumStudies=
+NumSNPs=
 
 $MEGASOFT_Options
 _method=mcmc
@@ -33,12 +36,22 @@ do
     DirOut=$DirOutput'posterior_'$NumStudies'_'$NumSNPs'_'$OS'_'$nthr".txt"
     DirLog=$DirOutput'Log_'$NumStudies'_'$NumSNPs'_'$OS'_'$nthr".log"
     
-    Script='sudo '$DirMega'
-    echo $DirLog
+    Script="sudo "$DirMEGA
+    Script=$Script" --input "$DirIn
+    Script=$Script" --pvalue_table "$DirHan
+    Script=$Script" --output "$DirOut
+    Script=$Script" --log "$DirLog
+    Script=$Script" --mvalue "
+    Script=$Script" --mvalue_method "$_method
+    Script=$Script" --mcmc_sample "$_samples
+    Script=$Script" --seed "$_seed
+    Script=$Script" --mvalue_prior_sigma "$_sigma
+    Script=$Script" --mvalue_prior_alpha "$_alpha
+    Script=$Script" --mvalue_prior_beta "$_beta
+    Script=$Script" --mvalue_p_thres "$_thres
+    Script=$Script" --thread "$nthr
 
-    
-
+    #echo $Script
+    $Script
 
 done
-
-
